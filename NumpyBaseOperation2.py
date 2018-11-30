393614dcc9830ea93bcf5a3c0dac8deb3f007630
Govern <745087337@qq.com>
2018-11-30 09:57:38 +0800

    "NumpyBaseOperation"

--- NumpyBaseOperation2.py
+++ NumpyBaseOperation2.py
@@ -13,6 +13,20 @@ print(np.mean(A))
 print(np.average(A))
 
 #求中位数的函数
-print(np.median())
+print(np.median(A))
 
+#累加函数
+print(np.cumsum(A))
 
+#累差运算函数 该函数计算的便是每一行中后一项与前一项之差
+print(np.diff(A))
+
+#nonzero()函数 这个函数将所有非零元素的行与列坐标分割开，重构成两个分别关于行和列的矩阵。
+print(np.nonzero(A))
+
+#矩阵转秩
+print(np.transpose(A))
+print(A.T)
+
+#clip函数  clip(Array,Array_min,Array_max)
+print(np.clip(A,5,9))
